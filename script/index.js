71ca09a267150f27534d7bbff3a1c05b25b0de96
Hiruge <okita1110@outlook.com>
2024-06-17 20:30:03 +0900

    [clean] jQueryのセレクタ複数指定に関するリファクタリング
    3行に渡って個別にタグを指定していたが、$(' , , ')とカンマで区切れば良かっただけなので、可読性向上のためこれを修正

--- script/index.js
+++ script/index.js
@@ -10,17 +10,11 @@ $(document).ready(function() {
         if($(this).prop('checked')){
             $('.ice-input-div').html(iceInputDivText);
             $('.ice-mode-show').show();
-            // <html>,<header>,<footer>にice-modeクラスを追加して、CSSでスタイルを変更する
-            $('html').addClass('ice-mode');
-            $('header').addClass('ice-mode');
-            $('footer').addClass('ice-mode');
+            $('html, header, footer').addClass('ice-mode'); // アイスモード時のスタイル変更用
         }else{
             $('.ice-input-div').html('');
             $('.ice-mode-show').hide();
-            // <html>,<header>,<footer>からice-modeクラスを削除して、CSSでスタイルを変更する
-            $('html').removeClass('ice-mode');
-            $('header').removeClass('ice-mode');
-            $('footer').removeClass('ice-mode');
+            $('html, header, footer').removeClass('ice-mode');  // アイスモード時のスタイル変更用
         }
     });
 
