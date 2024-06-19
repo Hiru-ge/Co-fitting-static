e8a8e74830b1a9079cb6fa76047debddb505485b
Hiruge <okita1110@outlook.com>
2024-06-19 10:46:00 +0900

    [todo] アイスモード時の、氷量も含めた合計量での変換機能を実装を要請するtodoコメントを追加
    アイスモード時、氷量は分けて倍率変換をしていたが、氷量も含めたトータルの量を最終目標量としたいケースが多かったため、これの実装を試みる

--- script/index.js
+++ script/index.js
@@ -135,6 +135,7 @@ $(document).ready(function() {
         `;
         let processOutput = DefaultProcessOutput;
         let totalWater_ml = 0;
+        // todo:アイスモード時、氷量も含めた合計量として変換を行えるようにする
         for (let i = 1; i <= pourTimes; i++) {
             // todo:算出とフォーマットが並行して行われてしまっているので、まず算出し、フォーマット用の関数に渡して整形するようにしたい
             let minutes = String($(`.pour-step${i}`).children('.minutes').val()).padStart(2, '0');
